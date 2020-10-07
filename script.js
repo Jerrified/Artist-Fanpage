c6623f59963597eb2a69443992b920ce9b842e1c
Jerrified <68245366+Jerrified@users.noreply.github.com>
2020-10-07 18:30:02 -0400

    Add

---
+++ script.js
@@ -0,0 +1,13 @@
+function RemoveForm() {
+    var IDofForm = "form";
+
+    var IDofDivWithForm = "reg";
+
+    document.getElementById(IDofForm) = "submit";
+
+    document.getElementById(IDofDivWithForm).style.visibility = "hidden";
+
+    if (document.getElementById(IDofForm) = "submit",document.getElementById(IDofDivWithForm).style.visibility = "hidden") {
+        alert("Your information has been confirmed!")
+    }
+}
