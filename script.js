988d150619aba581432c955f05f9b6e2a130e56e
Jerrified <68245366+Jerrified@users.noreply.github.com>
2020-10-08 17:56:07 -0400

    Finished landing page

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function RemoveForm() {
-    var IDofForm = "form";
-
-    var IDofDivWithForm = "reg";
-
-    document.getElementById(IDofForm) = "submit";
-
-    document.getElementById(IDofDivWithForm).style.visibility = "hidden";
-
-    if (document.getElementById(IDofForm) = "submit",document.getElementById(IDofDivWithForm).style.visibility = "hidden") {
-        alert("Your information has been confirmed!")
-    }
+function Submit() {
+    alert("Your information has been submitted!")
 }
